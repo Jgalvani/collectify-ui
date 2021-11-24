faea27c3a98b9f47b3753503db51ceb3df5dae04
jgalvani <jchristophegalvani@gmail.com>
2021-11-24 20:02:58 +0100

    fix: missing '/' at the end of API_URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://8d80-2a01-cb20-4806-7d00-68b4-e7a6-1f8c-2ac1.ngrok.io',
+  API_URL: 'https://8d80-2a01-cb20-4806-7d00-68b4-e7a6-1f8c-2ac1.ngrok.io/',
   TOKEN: '821020a53c4ef5636151fc7ff35c640e4c9881e2',
 };
 
