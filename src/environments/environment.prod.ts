b5ca42a6e9c4d99e7798d0aafb9542cac856fcde
jgalvani <jchristophegalvani@gmail.com>
2021-11-24 19:52:43 +0100

    fix: new API_URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://f435-37-173-41-192.ngrok.io/',
+  API_URL: 'https://8d80-2a01-cb20-4806-7d00-68b4-e7a6-1f8c-2ac1.ngrok.io',
   TOKEN: '821020a53c4ef5636151fc7ff35c640e4c9881e2',
 };
